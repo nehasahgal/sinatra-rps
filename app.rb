bcbeee9534603bd027a43f2d1029b97b930f2033
nehasahgal <146212872+nehasahgal@users.noreply.github.com>
2023-10-16 17:41:44 +0000

    homepage built

--- app.rb
+++ app.rb
@@ -2,8 +2,26 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+ erb(:homepage)
+ # "
+ # <h1>Welcome to your Sinatra App!</h1>
+ # <p>Define some routes in app.rb</p>
+ # "
+end
+
+get("/rock") do
+ # @ourplay = ["rock", "paper", "scissors"]
+ # @theirplay = ["rock", "paper", "scissors"]
+
+ # if @ourplay == @theirplay
+ #   @outcome = "We lost!"
+
+   
+ erb(:rock)
+end
+
+get("/paper") do
+end
+
+get("/scissors") do
 end
